b75e2af07d9e25f740849adda7b5712bc32a2160
TomasReisenman <tom.manrey@gmail.com>
2018-11-18 12:48:38 -0300

    main models stablished

---
+++ OrtApp/OrtApp/Models/Video.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OrtApp.Models
+{
+    public class Video
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public string ImgUrl { get; set; }
+        public string VideoUrl { get; set; }
+
+        public Usuario UsuarioID { get; set; }
+
+        public virtual Usuario Usuario { get; set; }
+    }
+}
