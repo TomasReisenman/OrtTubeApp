b6cbb7c706b5d45c02848c276f86562f497c156c
TOMAS MARIANO REISENMAN <37752808@ort>
2018-11-26 21:59:35 -0300

    APROBAMOS !

--- OrtApp/OrtApp/Models/Usuario.cs
+++ OrtApp/OrtApp/Models/Usuario.cs
@@ -9,6 +9,7 @@ namespace OrtApp.Models
     public class Usuario
     {
   
+
         public int ID { get; set; }
         public string Nombre { get; set; }
         public string Password { get; set; }
